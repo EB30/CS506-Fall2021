312be030559875928ef37104f811e8fc26155bf4
William Fang <wfang@bu.edu>
2021-09-30 15:03:49 -0400

    solved distance functions and cos sim

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,12 +5,26 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        inter = (x[i]- y[i])
+        res+= (inter if inter>=0 else -1*inter) #Abs
+    return res
 
-def jaccard_dist(x, y):
-    raise NotImplementedError()
+
+def jaccard_dist(x, y): #We assume they are the same length
+    xds = 0
+    inter = 0
+    for i in len(range(x)):
+        if (x[i]==y[i]):
+            inter+=1
+        xds +=x[i]
+    return 1.0- inter/xds
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    sum = 0
+    for i in range(len(x)):
+        sum += x[i] *y[i]
+    return sum/(len(x) *len(y))
 
 # Feel free to add more
